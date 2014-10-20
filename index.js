89b57bfa8082749ec9665ad6b4784543c2097f1f
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2014-10-20 15:02:51 +0200

    moved public website into /public. Node.js code is private.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public/'));
 
 var config = [
     {
