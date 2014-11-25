47e14f76177e4cad6c94f7180b2ea0bdf156adb9
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2014-11-26 00:29:00 +0100

    added CORS for chrome, when deployed to sheltered-lowlands on heroku

--- roa4node.js
+++ roa4node.js
@@ -269,7 +269,7 @@ function checkBasicAuthentication(req, res, next) {
 }
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'sheltered-lowlands-3555.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://sheltered-lowlands-3555.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
